755bf9f192b9e440737cc898a4fc943d6ba78bce
lucho03 <l.valentinov.ivanov@gmail.com>
2024-11-24 22:35:25 +0200

    Show number of coins which are used

--- Greedy algorithm/greedy.c
+++ Greedy algorithm/greedy.c
@@ -3,8 +3,11 @@
 int i = 0;
 int cents[] = { 50, 20, 10, 5, 2, 1 };
 void calculate( int amount ) {
-    printf( "Number of %d-cent units: %d\n", cents[i], (amount / cents[i]) );
-    
+    int change = amount / cents[i];
+    if( change != 0 ) {
+        printf( "Number of %d-cent units: %d\n", cents[i], change );
+    }
+
     amount = amount % cents[i];
     if( amount == 0 ) {
         return;
